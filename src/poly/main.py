a909451d376b60d3f0b02b1b2f5505014b1a0dca
waiyan13 <zaw.waiyan.lwin96@gmail.com>
2023-07-13 21:47:17 +0630

    feat[Route]: Add route to get app version

--- src/poly/main.py
+++ src/poly/main.py
@@ -8,3 +8,8 @@ app = FastAPI()
 @app.get("/")
 async def root(user=Depends(get_current_auth_user)):
     return {"message": "Welcome to Poly"}
+
+
+@app.get("/version")
+async def version():
+    return {"message": "Poly 0.1.0"}
