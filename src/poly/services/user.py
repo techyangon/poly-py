7fff7d9bee0dd899de76cf27d38a4e42a8d28dd9
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2023-12-01 12:00:16 +0630

    feat[DB]: Get user by email or name

--- src/poly/services/user.py
+++ src/poly/services/user.py
@@ -4,7 +4,13 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from poly.db.models import User
 
 
-async def get_user(email: str, session: AsyncSession) -> User | None:
+async def get_user_by_email(email: str, session: AsyncSession) -> User | None:
     query = select(User).where(User.email == email)
+    result = await session.scalars(query)
+    return result.one_or_none()
+
+
+async def get_user_by_name(name: str, session: AsyncSession) -> User | None:
+    query = select(User).where(User.name == name)
     result = await session.scalars(query)
     return result.one_or_none()
