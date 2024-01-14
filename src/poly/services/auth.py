36363b4ba83fcfcdc4a8fa522a32ff967e6abb2a
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2024-01-14 22:21:20 +0630

    feat[Auth]: Add function to check permission for path access

--- src/poly/services/auth.py
+++ src/poly/services/auth.py
@@ -1,15 +1,17 @@
 from datetime import datetime, timedelta
-from typing import Annotated, Mapping
+from typing import Annotated, Literal, Mapping
 
-from fastapi import Cookie, Depends, Header, HTTPException, status
+from casbin import AsyncEnforcer
+from fastapi import Cookie, Depends, Header, HTTPException, Request, status
 from jose import jwt
 from jose.exceptions import ExpiredSignatureError, JWTClaimsError, JWTError
 from passlib.context import CryptContext
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from poly.config import Settings, get_settings
 from poly.db import get_session
 from poly.db.models import User
+from poly.rbac.models import get_enforcer
 from poly.services import oauth2_scheme
 from poly.services.user import get_user_by_email, get_user_by_name
 
@@ -64,8 +66,8 @@ def generate_token(
     return encoded_jwt
 
 
-async def authenticate(email: str, password: str, session: AsyncSession) -> User:
-    user = await get_user_by_email(email=email, session=session)
+async def authenticate(email: str, password: str, session: async_sessionmaker) -> User:
+    user = await get_user_by_email(email=email, async_session=session)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -80,48 +82,32 @@ async def authenticate(email: str, password: str, session: AsyncSession) -> User
     return user
 
 
-async def validate_username(username: str, session: AsyncSession):
-    user = await get_user_by_name(name=username, session=session)
+async def get_active_user(
+    x_username: Annotated[str, Header()],
+    session: Annotated[async_sessionmaker, Depends(get_session)],
+) -> User:
+    user = await get_user_by_name(name=x_username, async_session=session)
+
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="User not found",
         )
+
     if not user.is_active:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Inactive user",
         )
+
     return user
 
 
-async def get_active_user(
-    x_username: Annotated[str, Header()],
-    token: Annotated[str, Depends(oauth2_scheme)],
-    session: Annotated[AsyncSession, Depends(get_session)],
+async def validate_cookie(
     settings: Annotated[Settings, Depends(get_settings)],
-) -> User:
-    if not token:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Empty token"
-        )
-
-    claims = validate_jwt(
-        audience=settings.access_token_audience,
-        issuer=settings.access_token_issuer,
-        secret=settings.secret_key,
-        subject=x_username,
-        token=token,
-    )
-    return await validate_username(username=claims["sub"], session=session)
-
-
-async def get_active_user_from_cookie(
-    x_username: Annotated[str, Header()],
-    session: Annotated[AsyncSession, Depends(get_session)],
-    settings: Annotated[Settings, Depends(get_settings)],
+    user: Annotated[User, Depends(get_active_user)],
     poly_refresh_token: Annotated[str | None, Cookie()] = None,
-) -> User:
+) -> str:  # pragma: no cover
     if not poly_refresh_token:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Empty token"
@@ -131,7 +117,38 @@ async def get_active_user_from_cookie(
         audience=settings.access_token_audience,
         issuer=settings.access_token_issuer,
         secret=settings.secret_key,
-        subject=x_username,
+        subject=user.name,
         token=poly_refresh_token,
     )
-    return await validate_username(username=claims["sub"], session=session)
+
+    return claims["sub"]
+
+
+async def validate_access_token(
+    settings: Annotated[Settings, Depends(get_settings)],
+    token: Annotated[str, Depends(oauth2_scheme)],
+    user: Annotated[User, Depends(get_active_user)],
+) -> str:  # pragma: no cover
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Empty token",
+        )
+
+    claims = validate_jwt(
+        audience=settings.access_token_audience,
+        issuer=settings.access_token_issuer,
+        secret=settings.secret_key,
+        subject=user.name,
+        token=token,
+    )
+
+    return claims["sub"]
+
+
+async def check_permission(
+    request: Request,
+    username: Annotated[str, Depends(validate_access_token)],
+    enforcer: Annotated[AsyncEnforcer, Depends(get_enforcer)],
+) -> Literal[True, False]:  # pragma: no cover
+    return enforcer.enforce(username, request.url.path.strip("/"), request.method)
