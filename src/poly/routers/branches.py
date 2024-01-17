17f1d432c233a73aa5f94eb100bd5c1b8e8dcec6
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2024-01-17 21:16:43 +0630

    feat[Route]: Add response for empty table

--- src/poly/routers/branches.py
+++ src/poly/routers/branches.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from poly.db import get_session
@@ -18,7 +18,13 @@ async def get_paginated_branches(
     skip: int = 0,
     limit: int = 10,
 ):
-    branches = await get_branches(skip=skip, per_page=limit, async_session=session)
     total = await get_branches_count(async_session=session)
+
+    if not total:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="There are no branches."
+        )
+
+    branches = await get_branches(skip=skip, per_page=limit, async_session=session)
 
     return {"branches": branches, "total": total}
