62c12506fcd3ee0a64b3693404657acd5ae41e57
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2024-01-20 22:08:03 +0630

    refactor[Service]: Remove duplicate function

--- src/poly/routers/resources.py
+++ src/poly/routers/resources.py
@@ -4,9 +4,9 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from poly.db import get_session
-from poly.db.schema import Resources
+from poly.db.schema import Resource, Resources
 from poly.services.permissions import check_permission
-from poly.services.resources import get_resources
+from poly.services.resources import get_all_resources
 
 router = APIRouter(prefix="/resources", tags=["resources"])
 
@@ -14,8 +14,9 @@ router = APIRouter(prefix="/resources", tags=["resources"])
 @router.get("/", response_model=Resources)
 async def get_paginated_resources(
     session: Annotated[async_sessionmaker, Depends(get_session)],
-    _: Annotated[bool, Depends(check_permission)],
+    _: Annotated[str, Depends(check_permission)],
 ):
-    resources = await get_resources(async_session=session)
+    resource_result = await get_all_resources(async_session=session)
+    resources = [Resource(name=resource.name) for resource in resource_result]
 
     return {"resources": resources}
--- src/poly/services/resources.py
+++ src/poly/services/resources.py
@@ -4,14 +4,6 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from poly.db.models import Resource
-from poly.db.schema import Resource as ResourceResponse
-
-
-async def get_resources(async_session: async_sessionmaker) -> list[ResourceResponse]:
-    async with async_session() as session, session.begin():
-        result = await session.scalars(select(Resource).order_by(Resource.created_at))
-        resources = result.all()
-        return [ResourceResponse(name=resource.name) for resource in resources]
 
 
 async def get_all_resources(
