894d2e93b23011ef8733884a1eb21d5f4c9f606d
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2024-01-31 17:27:05 +0630

    feat[Route]: Add endpoint for getting user role and permissions

--- src/poly/main.py
+++ src/poly/main.py
@@ -3,13 +3,14 @@ from typing import Annotated
 from fastapi import Depends, FastAPI
 
 from poly import __version__
-from poly.routers import auth, branches, locations, resources, roles, user
+from poly.routers import auth, branches, locations, permissions, resources, roles, user
 from poly.services.auth import validate_access_token
 
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(branches.router)
 app.include_router(locations.router)
+app.include_router(permissions.router)
 app.include_router(resources.router)
 app.include_router(roles.router)
 app.include_router(user.router)
