73d3beadbb34ceec94584330e41e8e201f021399
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2024-02-10 22:13:48 +0630

    refactor[Casbin]: Change method signature for enforcer

--- src/poly/rbac/models.py
+++ src/poly/rbac/models.py
@@ -1,26 +1,20 @@
-from typing import Annotated
-
 from casbin import AsyncEnforcer, Model
 from casbin_async_sqlalchemy_adapter import Adapter
-from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncEngine
 
-from poly.config import Settings, get_rbac_models, get_settings
-from poly.db import get_engine
+from poly.config import get_rbac_models
 
 
-async def get_enforcer(
-    settings: Annotated[Settings, Depends(get_settings)]
-):  # pragma: no cover
+async def get_enforcer(engine: AsyncEngine):  # pragma: no cover
     model = Model()
     model.load_model_from_text(text=get_rbac_models())
 
     enforcer = AsyncEnforcer(model=model)
 
-    async with get_engine(settings=settings) as engine:
-        adapter = Adapter(engine=engine, warning=False)
-        await adapter.create_table()
+    adapter = Adapter(engine=engine, warning=False)
+    await adapter.create_table()
 
-        enforcer.set_adapter(adapter)
-        await enforcer.load_policy()
+    enforcer.set_adapter(adapter)
+    await enforcer.load_policy()
 
     return enforcer
